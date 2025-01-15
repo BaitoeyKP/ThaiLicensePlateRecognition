795c0a037e3ff094c4b29ccafb349a36f9406b1a
BaitoeyKP <kanyarat.phookosot@gmail.com>
2025-01-15 17:21:23 +0700

    change file input

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+import cv2
 import json
 
 from imageEnhancement import imageEnhancement
-from loadedImage import loadImageFromFolder
 from resizeImage import resizeImageFix
 from runOnnxModel import runOnnxModel
 from segmentationCharacters import segmentationCharacters
 from segmentationProvince import segmentationProvince
 from segmentationRow import segmentationRow
 
-folder_path = "../testmodel"
 character_model_path = "model/Characters_MobileNetV3Small.onnx"
 province_model_path = "model/Province_MobileNetV3Small.onnx"
 characters_class_mapping = [
@@ -147,53 +146,49 @@ province_class_mapping = [
     "สมุทรสงคราม",
     "เบตง",
 ]
-loaded_images, filenames = loadImageFromFolder(folder_path)
-for img, filename in zip(loaded_images, filenames):
-    # print(f"ParkingSlots_ID : {filename}")
-    enhance_image = imageEnhancement(img)
-    data, province = segmentationRow(enhance_image)
 
-    charactersCrop = segmentationCharacters(data, filename)
-    characters = []
-    if charactersCrop is not None:
-        for img in charactersCrop:
-            if img is not None and img.size > 0:
-                height = 224
-                width = height // 3  # 3:1 ratio
-                img = resizeImageFix(img, width, height)
-                character, confident = runOnnxModel(
-                    img,
-                    character_model_path,
-                    characters_class_mapping,
-                )
-                # print(f"character : {character} | confident : {confident:.2f}%")
-                characters.append(character)
-    characters = "".join(characters)
-    # print(f"License_ID : {characters}")
 
-    provinceCrop = segmentationProvince(province, filename)
-    width = 224
-    height = width // 3  # 1:3 ratio
-    if provinceCrop:
-        provinceImage = resizeImageFix(provinceCrop[0], width, height)
-        province, confident = runOnnxModel(
-            provinceImage,
-            province_model_path,
-            province_class_mapping,
-        )
-        if confident > 50:
-            province = province
-        else:
-            province = ""
-        # print(f"province : {province} | confident : {confident:.2f}%")
-        # print(f"Province : {province}")
+filename = "24_02_05_V00490.jpg"
+img = cv2.imread(filename)
+enhance_image = imageEnhancement(img)
+data, province = segmentationRow(enhance_image)
 
-    output = {
-        "ParkingSlots_ID": filename,
-        "License_ID": characters,
-        "Province": province,
-    }
+charactersCrop = segmentationCharacters(data, filename)
+characters = []
+if charactersCrop is not None:
+    for img in charactersCrop:
+        if img is not None and img.size > 0:
+            height = 224
+            width = height // 3  # 3:1 ratio
+            img = resizeImageFix(img, width, height)
+            character, confident = runOnnxModel(
+                img,
+                character_model_path,
+                characters_class_mapping,
+            )
+            characters.append(character)
+characters = "".join(characters)
 
-    # Convert results to JSON and print
-    json_output = json.dumps(output, ensure_ascii=False, indent=2)
-    print(json_output)
+provinceCrop = segmentationProvince(province, filename)
+width = 224
+height = width // 3  # 1:3 ratio
+if provinceCrop:
+    provinceImage = resizeImageFix(provinceCrop[0], width, height)
+    province, confident = runOnnxModel(
+        provinceImage,
+        province_model_path,
+        province_class_mapping,
+    )
+    if confident > 50:
+        province = province
+    else:
+        province = ""
+
+output = {
+    "ParkingSlots_ID": filename,
+    "License_ID": characters,
+    "Province": province,
+}
+
+json_output = json.dumps(output, ensure_ascii=False, indent=2)
+print(json_output)
