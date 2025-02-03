603f50f283db8b7fc6ab42de12310a54b4bc3f98
BaitoeyKP <kanyarat.phookosot@gmail.com>
2025-02-03 20:14:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from segmentationCharacters import segmentationCharacters
 from segmentationProvince import segmentationProvince
 from segmentationRow import segmentationRow
 
-folder_path = "C:/Users/AVI003/Downloads/LicensePlate"
+folder_path = "../dataset/20220531"
 
 loaded_images, filenames = loadImageFromFolder(folder_path)
 for img, filename in zip(loaded_images, filenames):
     enhance_image = imageEnhancement(img)
     data, province = segmentationRow(enhance_image)
-    charactersCrop = segmentationCharacters(data, filename, save_path="C:/Users/AVI003/Downloads/LicensePlate/label/characters")
-    provinceCrop = segmentationProvince(province, filename, save_path="C:/Users/AVI003/Downloads/LicensePlate/label/province")
+    charactersCrop = segmentationCharacters(
+        data, filename, save_path="../output/characters"
+    )
+    provinceCrop = segmentationProvince(
+        province, filename, save_path="../output/province"
+    )
