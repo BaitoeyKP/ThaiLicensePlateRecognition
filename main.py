ae1d509034eedccb50a03c725ef7cf5103cbf293
BaitoeyKP <kanyarat.phookosot@gmail.com>
2025-02-13 22:33:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,31 +7,31 @@ from segmentationCharacters import segmentationCharacters
 from segmentationProvince import segmentationProvince
 from segmentationRow import segmentationRow
 
-folder_path = "../dataset/20220602/L1"
+folder_path = "../dataset/internet"
 
-loaded_images, filenames = loadImageFromFolder(folder_path)
-for img, filename in zip(loaded_images, filenames):
-    enhance_image = imageEnhancement(img)
-    data, province = segmentationRow(
-        enhance_image,
-    )
-    charactersCrop = segmentationCharacters(
-        data, filename, save_path="../output/characters"
-    )
-    provinceCrop = segmentationProvince(
-        province, filename, save_path="../output/province"
-    )
+# loaded_images, filenames = loadImageFromFolder(folder_path)
+# for img, filename in zip(loaded_images, filenames):
+#     enhance_image = imageEnhancement(img)
+#     data, province = segmentationRow(
+#         enhance_image,
+#     )
+#     charactersCrop = segmentationCharacters(
+#         data, filename, save_path="../output/characters"
+#     )
+#     provinceCrop = segmentationProvince(
+#         province, filename, save_path="../output/province"
+#     )
 
-# filename = "7809975aef6a2d2d78f36b8d0a27d090.jpg"
-# file_path = os.path.join(folder_path, filename)
-# img = cv2.imread(file_path)
-# enhance_image = imageEnhancement(img)
-# data, province = segmentationRow(enhance_image, True)
-# charactersCrop = segmentationCharacters(
-#     data,
-#     filename,
-#     # save_path="../output/characters",
-# )
-# provinceCrop = segmentationProvince(
-#     province, filename, True, save_path="../output/province"
-# )
+filename = "images (4).jpg"
+file_path = os.path.join(folder_path, filename)
+img = cv2.imread(file_path)
+enhance_image = imageEnhancement(img)
+data, province = segmentationRow(enhance_image, True)
+charactersCrop = segmentationCharacters(
+    data,
+    filename,
+    # save_path="../output/characters",
+)
+provinceCrop = segmentationProvince(
+    province, filename, True, save_path="../output/province"
+)
