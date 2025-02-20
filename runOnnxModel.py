a3d0f43af540c75824f37d0f31f8779e6b1db347
BaitoeyKP <kanyarat.phookosot@gmail.com>
2025-02-20 19:16:25 +0700

    return error

--- runOnnxModel.py
+++ runOnnxModel.py
@@ -10,40 +10,46 @@ def softmax(x):
 
 
 def runOnnxModel(image_input, model_path, class_mapping):
-    session = ort.InferenceSession(model_path)
+    try:
+        session = ort.InferenceSession(model_path)
 
-    if isinstance(image_input, str):
-        image = cv2.imread(image_input)
-    elif isinstance(image_input, np.ndarray):
-        image = image_input
-    elif isinstance(image_input, Image.Image):
-        image = cv2.cvtColor(np.array(image_input), cv2.COLOR_RGB2BGR)
-    else:
-        raise ValueError("Unsupported image input type")
+        if isinstance(image_input, str):
+            image = cv2.imread(image_input)
+        elif isinstance(image_input, np.ndarray):
+            image = image_input
+        elif isinstance(image_input, Image.Image):
+            image = cv2.cvtColor(np.array(image_input), cv2.COLOR_RGB2BGR)
+        else:
+            # raise ValueError("Unsupported image input type")
+            return "", 0
 
-    input_data = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) / 255.0
-    input_data = np.transpose(input_data, (2, 0, 1))  
-    input_data = np.expand_dims(input_data, axis=0).astype(np.float32)
+        input_data = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) / 255.0
+        input_data = np.transpose(input_data, (2, 0, 1))
+        input_data = np.expand_dims(input_data, axis=0).astype(np.float32)
 
-    input_name = session.get_inputs()[0].name
-    outputs = session.run(None, {input_name: input_data.astype(np.float32)})
-    logits = outputs[0][0]
+        input_name = session.get_inputs()[0].name
+        outputs = session.run(None, {input_name: input_data.astype(np.float32)})
+        logits = outputs[0][0]
 
-    probabilities = softmax(logits)
+        probabilities = softmax(logits)
 
-    top_indices = np.argsort(probabilities)[-3:][::-1]
+        top_indices = np.argsort(probabilities)[-3:][::-1]
 
-    # print("\nPredictions:")
-    # for idx in top_indices:
-    #     class_label = (
-    #         class_mapping[idx] if idx < len(class_mapping) else f"Unknown({idx})"
-    #     )
-    #     print(
-    #         f"Index {idx}: Class {class_label}, "
-    #         f"Confidence: {probabilities[idx]*100:.2f}%"
-    #     )
+        # print("\nPredictions:")
+        # for idx in top_indices:
+        #     class_label = (
+        #         class_mapping[idx] if idx < len(class_mapping) else f"Unknown({idx})"
+        #     )
+        #     print(
+        #         f"Index {idx}: Class {class_label}, "
+        #         f"Confidence: {probabilities[idx]*100:.2f}%"
+        #     )
 
-    class_label = class_mapping[top_indices[0]]
-    confident = probabilities[top_indices[0]] * 100
+        class_label = class_mapping[top_indices[0]]
+        confident = probabilities[top_indices[0]] * 100
 
-    return class_label, confident
+        return class_label, confident
+
+    except Exception as e:
+        print(f"Error during running model{str(e)}")
+        return "", 0
